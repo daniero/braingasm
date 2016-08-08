9a07fc447ccda39e3f578043cf110e1721ae8aff
Daniel Rødskog <danielmero@gmail.com>
2016-08-08 21:35:12 +0200

    Track line numbers while tokenizing input

--- lib/braingasm/tokenizer.rb
+++ lib/braingasm/tokenizer.rb
@@ -1,10 +1,24 @@
+require 'strscan'
+
 module Braingasm
   class Tokenizer < Enumerator
     attr_accessor :input
+    attr_reader :line_numer
 
     def initialize(input)
+      @line_numer = 1
+
+      scanner = StringScanner.new(input)
+
       super() do |y|
-        input.scan(/\S/) { |token| y << token }
+        loop do
+          while scanner.skip(/\s/)
+            @line_numer += 1 if scanner.beginning_of_line?
+          end
+
+          break if scanner.eos?
+          y << scanner.scan(/\S/)
+        end
       end
     end
   end
--- spec/tokenizer_spec.rb
+++ spec/tokenizer_spec.rb
@@ -18,6 +18,27 @@ module Braingasm
         expect(subject.next).to be == '>'
         expect { subject.next }.to raise_error StopIteration
       end
+
+      it "handles empty input" do
+        @input = ""
+
+        expect { subject.next }.to raise_error StopIteration
+      end
+
+      describe :line_numer do
+        it "returns the line number from which the previous token was read" do
+          @input = " a \nbc\n d"
+
+          subject.next
+          expect(subject.line_numer).to be 1
+          subject.next
+          expect(subject.line_numer).to be 2
+          subject.next
+          expect(subject.line_numer).to be 2
+          subject.next
+          expect(subject.line_numer).to be 3
+        end
+      end
   end
 
 end
